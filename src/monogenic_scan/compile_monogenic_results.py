22fd57f68c365c5475f369b9767aa254551ca6cf
Megan Le <meganlecs@gmail.com>
2024-05-28 14:58:31 -0500

    fix scan compilation bug

--- src/monogenic_scan/compile_monogenic_results.py
+++ src/monogenic_scan/compile_monogenic_results.py
@@ -55,8 +55,8 @@ pert_stats = np.load(scan_prefix + "pertStat0.npy")
 
 total_sample_sizes = [len(np.load(config['paths']['source1_indices'])), len(np.load(config['paths']['source2_indices'])), len(np.load(config['paths']['target_indices']))]
 
-file_names = ["p_vals", "stats", "targetFreq", "source1Freq", "source2Freq", "expected", "expectedH0", "source1SampleSize", "source2SampleSize", "targetSampleSize", "s1H0", "s2H0", "h1NegLog", "h0NegLog", "pertP", "pertStat"]
-arr_names = [p_vals, stats, t_freqs, s1_freqs, s2_freqs, expected, expected_h0, s1_n, s2_n, t_n, s1_h0s, s2_h0s, h1_neg_logs, h0_neg_logs, pert_p, pert_stats]
+file_names = ["source1Freq", "source2Freq", "targetFreq", "expected", "s1H0", "s2H0", "expectedH0", "h1NegLog", "h0NegLog", "stats", "p_vals", "pertStat", "pertP", "source1SampleSize", "source2SampleSize", "targetSampleSize"]
+arr_names = [s1_freqs, s2_freqs, t_freqs, expected, s1_h0s, s2_h0s, expected_h0, h1_neg_logs, h0_neg_logs, stats, p_vals, pert_stats, pert_p, s1_n, s2_n, t_n]
 
 # load all result arrays
 i = 0
@@ -64,7 +64,6 @@ while i < num_chunks - 1:
     for j in range(len(file_names)):
         file_name = scan_prefix + file_names[j] + str(start_indices[i]) + ".npy"
         arr_names[j] = np.append(arr_names[j], np.load(file_name))
-
     i += 1
 
 # calculate non-centrality parameter
@@ -76,14 +75,22 @@ out_file = open(out_path, 'w')
 out_file.write("CHROM\tPOSITION\tREF\tALT\tSOURCE1_FREQ\tSOURCE2_FREQ\tTARGET_FREQ\tEXPECTED\tS1_H0\tS2_H0\tEXPECTED_H0\tH1_NEG_LOG\tH0_NEG_LOG\tSTATISTIC\tP_VALUE\tPERT_STAT\tPERT_P\tNCX2_P\tS1_N\tS2_N\tT_N\n")
 
 j = 0
-while j < len(p_vals):
+while j < len(arr_names[0]):
     chrom_line = chros_file.readline()
     chrom_split = chrom_line.split()
 
-    out = chrom_line.rstrip() + "\t" + str(s1_freqs[j]) + "\t" + str(s2_freqs[j]) + "\t" + str(t_freqs[j]) + "\t" + str(expected[j]) + "\t" + str(s1_h0s[j]) + "\t" + str(s2_h0s[j]) + "\t" + str(expected_h0[j]) + "\t" + str(h1_neg_logs[j]) + "\t" + str(h0_neg_logs[j]) + "\t" + str(stats[j]) + "\t" + str(p_vals[j]) + "\t" + str(pert_stats[j]) + "\t" + str(pert_p[j]) + "\t" + str(ncx2.sf(pert_stats[j], 1, lam)) + "\t" + str(int(s1_n[j])) + "\t" + str(int(s2_n[j])) + "\t" + str(int(t_n[j])) + "\n" 
+    out_file.write(chrom_line.rstrip() + '\t')
+    for k in range(13):
+        out_file.write(str(arr_names[k][j]) + '\t')
 
-    out_file.write(out)
-    
+    # writing the corrected perturbed statistic
+    out_file.write(str(ncx2.sf(arr_names[11][j], 1, lam)))
+
+    # write sample sizes
+    for k in range(13, 16):
+        out_file.write('\t' + str(arr_names[k][j]))
+
+    out_file.write('\n')
     j += 1
 
 chros_file.close()
